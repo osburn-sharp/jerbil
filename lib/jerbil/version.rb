01fa344f9680263f6fe6adecdc64300f44678753
Robert Sharp <robert@osburn-sharp.ath.cx>
2011-04-17 09:11:39 +0100

    Further tweaks to initscript install option

--- lib/jerbil/version.rb
+++ lib/jerbil/version.rb
@@ -1,9 +1,9 @@
 # Created by Jevoom
 #
 # 17-Apr-2011
-#   Tweak to init script for install option
+#   Further tweaks to initscript install option
 
 class Jerbil
-  Version = '0.0.7'
+  Version = '0.0.8'
   Version_Date = '17-Apr-2011'
 end
