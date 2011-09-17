e649e819b0793bfedd60ed2a4b7a2560d64490b0
Robert Sharp <robert@osburn-sharp.ath.cx>
2011-09-17 08:06:56 +0100

    Correct uid in jerbs install

--- lib/jerbil/version.rb
+++ lib/jerbil/version.rb
@@ -4,7 +4,7 @@
 #   Correct uid in jerbs install
 
 module Jerbil
-  Version = '0.2.1_beta1'
+  Version = '0.2.2'
   Version_Date = '17-Sep-2011'
-  Ident = 'jerbil-0.2.1_beta1 17-Sep-2011'
+  Ident = 'jerbil-0.2.2 17-Sep-2011'
 end
