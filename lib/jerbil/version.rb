0c5af3870620460f74e737cd53c80139b6cad81c
Robert Sharp <robert@osburn-sharp.ath.cx>
2011-09-17 08:24:51 +0100

    Adjust RUBYOPT etc in runscript

--- lib/jerbil/version.rb
+++ lib/jerbil/version.rb
@@ -1,10 +1,10 @@
 # Created by Jevoom
 #
 # 17-Sep-2011
-#   Add chmod to install
+#   Adjust RUBYOPT etc in runscript
 
 module Jerbil
-  Version = '0.2.4'
+  Version = '0.2.5'
   Version_Date = '17-Sep-2011'
-  Ident = 'jerbil-0.2.4 17-Sep-2011'
+  Ident = 'jerbil-0.2.5 17-Sep-2011'
 end
