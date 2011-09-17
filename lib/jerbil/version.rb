1f7340c21f44975a1b0d71fa52b5a3a2904b0fcf
Robert Sharp <robert@osburn-sharp.ath.cx>
2011-09-17 08:15:19 +0100

    Correct runscript!

--- lib/jerbil/version.rb
+++ lib/jerbil/version.rb
@@ -1,10 +1,10 @@
 # Created by Jevoom
 #
 # 17-Sep-2011
-#   Correct uid in jerbs install
+#   Correct runscript!
 
 module Jerbil
-  Version = '0.2.2'
+  Version = '0.2.3'
   Version_Date = '17-Sep-2011'
-  Ident = 'jerbil-0.2.2 17-Sep-2011'
+  Ident = 'jerbil-0.2.3 17-Sep-2011'
 end
