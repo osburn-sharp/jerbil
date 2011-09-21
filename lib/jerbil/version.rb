04ff5271882c0fe06594b2ee0ff67807fffa9a70
Robert Sharp <robert@osburn-sharp.ath.cx>
2011-09-21 08:20:01 +0100

    Add etc to gemspec

--- lib/jerbil/version.rb
+++ lib/jerbil/version.rb
@@ -1,12 +1,10 @@
 # Created by Jevoom
 #
 # 21-Sep-2011
-#   Include new jerbs command based on thor, and change Jerbil.remove so that it
-#   attempts to remove a service from remote servers even if it is not registered
-#   locally.
+#   Add 'etc' to gemspec
 
 module Jerbil
-  Version = '0.2.6'
+  Version = '0.2.7'
   Version_Date = '21-Sep-2011'
-  Ident = 'jerbil-0.2.6 21-Sep-2011'
+  Ident = 'jerbil-0.2.7 21-Sep-2011'
 end
