203459236f1f9bb90e4ed2e778338fdd8a2969f0
Robert Sharp <robert@osburn-sharp.ath.cx>
2012-07-23 12:04:38 +0100

    minor tweaks to config files for installation

--- lib/jerbil/version.rb
+++ lib/jerbil/version.rb
@@ -1,13 +1,13 @@
 # Created by Jevoom
 #
 # 23-Jul-2012
-#     fixed Etc.getpwnam.name
+#     minor tweaks to config files for installation
 
 module Jerbil
-  # version set to 0.99.05
-  Version = '0.99.05'
+  # version set to 0.99.06
+  Version = '0.99.06'
   # date set to 23-Jul-2012
   Version_Date = '23-Jul-2012'
-  #ident string set to: jerbil-0.99.05 23-Jul-2012
-  Ident = 'jerbil-0.99.05 23-Jul-2012'
+  #ident string set to: jerbil-0.99.06 23-Jul-2012
+  Ident = 'jerbil-0.99.06 23-Jul-2012'
 end
