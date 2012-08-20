18d68b7fefa76418eb5b35d082b0d4a2aba5ee12
Robert Sharp <robert@osburn-sharp.ath.cx>
2012-08-20 07:38:34 +0100

    fixed cut-and-paste errors in changing group

--- lib/jerbil/version.rb
+++ lib/jerbil/version.rb
@@ -1,13 +1,13 @@
 # Created by Jevoom
 #
 # 20-Aug-2012
-#     fixed Chuser calls in sbin/jerbild
+#     fixed cut-and-paste errors in changing group
 
 module Jerbil
-  # version set to 1.0.9
-  Version = '1.0.9'
+  # version set to 1.0.10
+  Version = '1.0.10'
   # date set to 20-Aug-2012
   Version_Date = '20-Aug-2012'
-  #ident string set to: jerbil-1.0.9 20-Aug-2012
-  Ident = 'jerbil-1.0.9 20-Aug-2012'
+  #ident string set to: jerbil-1.0.10 20-Aug-2012
+  Ident = 'jerbil-1.0.10 20-Aug-2012'
 end
