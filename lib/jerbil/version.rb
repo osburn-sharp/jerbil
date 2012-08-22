2e18e1e5b714f853b5e84c59ff26b01da35943ec
Robert Sharp <robert@osburn-sharp.ath.cx>
2012-08-22 13:30:05 +0100

    Add log_daemon capability to init files

--- lib/jerbil/version.rb
+++ lib/jerbil/version.rb
@@ -1,13 +1,13 @@
 # Created by Jevoom
 #
 # 22-Aug-2012
-#     Add log_daemon capability to sbin/jserviced
+#     Add log_daemon capability to init files
 
 module Jerbil
-  # version set to 1.0.12
-  Version = '1.0.12'
+  # version set to 1.0.13
+  Version = '1.0.13'
   # date set to 22-Aug-2012
   Version_Date = '22-Aug-2012'
-  #ident string set to: jerbil-1.0.12 22-Aug-2012
-  Ident = 'jerbil-1.0.12 22-Aug-2012'
+  #ident string set to: jerbil-1.0.13 22-Aug-2012
+  Ident = 'jerbil-1.0.13 22-Aug-2012'
 end
