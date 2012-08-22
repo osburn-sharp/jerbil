668906d49a2c83e6ec6708a4031ef14ef1676249
Robert Sharp <robert@osburn-sharp.ath.cx>
2012-08-22 13:40:01 +0100

    Set log_daemon to true in sbin/jserviced

--- lib/jerbil/version.rb
+++ lib/jerbil/version.rb
@@ -1,13 +1,13 @@
 # Created by Jevoom
 #
 # 22-Aug-2012
-#     Add log_daemon capability to init files
+#     Set log_daemon to true in sbin/jserviced
 
 module Jerbil
-  # version set to 1.0.13
-  Version = '1.0.13'
+  # version set to 1.0.14
+  Version = '1.0.14'
   # date set to 22-Aug-2012
   Version_Date = '22-Aug-2012'
-  #ident string set to: jerbil-1.0.13 22-Aug-2012
-  Ident = 'jerbil-1.0.13 22-Aug-2012'
+  #ident string set to: jerbil-1.0.14 22-Aug-2012
+  Ident = 'jerbil-1.0.14 22-Aug-2012'
 end
