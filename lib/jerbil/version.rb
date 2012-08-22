fe4c107332d1eea25d59a45ae290e72c6b9b3377
Robert Sharp <robert@osburn-sharp.ath.cx>
2012-08-22 16:58:51 +0100

    add log_daemon to stop and status services

--- lib/jerbil/version.rb
+++ lib/jerbil/version.rb
@@ -1,13 +1,13 @@
 # Created by Jevoom
 #
 # 22-Aug-2012
-#     Daemons expects STDOUT to be open, so ensure it is!
+#     add log_daemon to stop and status services
 
 module Jerbil
-  # version set to 1.0.18
-  Version = '1.0.18'
+  # version set to 1.0.19
+  Version = '1.0.19'
   # date set to 22-Aug-2012
   Version_Date = '22-Aug-2012'
-  #ident string set to: jerbil-1.0.18 22-Aug-2012
-  Ident = 'jerbil-1.0.18 22-Aug-2012'
+  #ident string set to: jerbil-1.0.19 22-Aug-2012
+  Ident = 'jerbil-1.0.19 22-Aug-2012'
 end
