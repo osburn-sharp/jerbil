f66fb576c14767d4778299e0ff53a265c5be7c14
Robert Sharp <robert@osburn-sharp.ath.cx>
2013-08-28 16:38:03 +0100

    Add the export of LANG to support encodings

--- lib/jerbil/version.rb
+++ lib/jerbil/version.rb
@@ -1,13 +1,13 @@
 # Created by Jevoom
 #
 # 28-Aug-2013
-#   further changes to runscripts to not fiddle with PATH etc
+#   Add the export of LANG to support encodings
 
 module Jerbil
-  # version set to 1.2.9
-  Version = '1.2.9'
+  # version set to 1.2.10
+  Version = '1.2.10'
   # date set to 28-Aug-2013
   Version_Date = '28-Aug-2013'
-  #ident string set to: jerbil-1.2.9 28-Aug-2013
-  Ident = 'jerbil-1.2.9 28-Aug-2013'
+  #ident string set to: jerbil-1.2.10 28-Aug-2013
+  Ident = 'jerbil-1.2.10 28-Aug-2013'
 end
