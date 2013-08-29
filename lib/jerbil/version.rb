9c13e14fd3a0e579360df368b82c5f07210c7832
Robert Sharp <robert@osburn-sharp.ath.cx>
2013-08-29 12:44:41 +0100

    Fix minor error in runscript (status)

--- lib/jerbil/version.rb
+++ lib/jerbil/version.rb
@@ -1,13 +1,13 @@
 # Created by Jevoom
 #
 # 29-Aug-2013
-#   First release with properly multiplexed runscript files
+#   Fix minor error in runscript (status)
 
 module Jerbil
-  # version set to 1.3.0
-  Version = '1.3.0'
+  # version set to 1.3.1
+  Version = '1.3.1'
   # date set to 29-Aug-2013
   Version_Date = '29-Aug-2013'
-  #ident string set to: jerbil-1.3.0 29-Aug-2013
-  Ident = 'jerbil-1.3.0 29-Aug-2013'
+  #ident string set to: jerbil-1.3.1 29-Aug-2013
+  Ident = 'jerbil-1.3.1 29-Aug-2013'
 end
