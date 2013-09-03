e6756016a9e586a0aaed38ca750a3b925d3f75a9
Robert Sharp <robert@osburn-sharp.ath.cx>
2013-09-03 14:22:21 +0100

    Ensure jerbil methods are called on the server and not the server record.

--- lib/jerbil/version.rb
+++ lib/jerbil/version.rb
@@ -1,14 +1,13 @@
 # Created by Jevoom
 #
 # 03-Sep-2013
-#   Introduce Optplus version of the bin/jerbil command and add a couple of information
-#   methods to the server in support.
+#   Ensure jerbil methods are called on the server and not the server record.
 
 module Jerbil
-  # version set to 1.3.2
-  Version = '1.3.2'
+  # version set to 1.3.3
+  Version = '1.3.3'
   # date set to 03-Sep-2013
   Version_Date = '03-Sep-2013'
-  #ident string set to: jerbil-1.3.2 03-Sep-2013
-  Ident = 'jerbil-1.3.2 03-Sep-2013'
+  #ident string set to: jerbil-1.3.3 03-Sep-2013
+  Ident = 'jerbil-1.3.3 03-Sep-2013'
 end
