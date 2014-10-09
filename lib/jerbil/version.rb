24efa8a91cdc3fcf37530a3aa328ef09dc38fb55
Robert Sharp <robert@osburn-sharp.ath.cx>
2014-10-09 22:13:49 +0100

    Fix jeni installer error.

--- lib/jerbil/version.rb
+++ lib/jerbil/version.rb
@@ -1,14 +1,13 @@
 # Created by Jevoom
 #
-# 03-Aug-2014
-#   Relocate server discovery to Jerbil::Monitor. Tidy up tests and readmes. Allow for 
-#   gethostname not to return fqdn and use gethostbyname if this happens.
+# 09-Oct-2014
+#   Fix jeni installer error.
 
 module Jerbil
-  # version set to 1.4.1
-  Version = '1.4.1'
-  # date set to 03-Aug-2014
-  Version_Date = '03-Aug-2014'
-  #ident string set to: jerbil-1.4.1 03-Aug-2014
-  Ident = 'jerbil-1.4.1 03-Aug-2014'
+  # version set to 1.4.2
+  Version = '1.4.2'
+  # date set to 09-Oct-2014
+  Version_Date = '09-Oct-2014'
+  #ident string set to: jerbil-1.4.2 09-Oct-2014
+  Ident = 'jerbil-1.4.2 09-Oct-2014'
 end
