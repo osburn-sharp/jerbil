20fceca3c96e196adf467114ae846cffe946eda5
Robert Sharp <robert@osburn-sharp.ath.cx>
2014-10-13 22:27:52 +0100

    Fix Gemfile dependencies to please rubygems and remove thor as a dependency.

--- Gemfile
+++ Gemfile
@@ -1,14 +1,13 @@
 # @markup ruby
 #
 # general utilities
-gem 'netaddr'
-gem 'term-ansicolor'
-gem 'thor'
-gem 'daemons'
+gem 'netaddr', '~> 0'
+gem 'term-ansicolor', '~> 0'
+gem 'daemons', '~> 0'
 
 # jerbil-related utilities
-gem 'jellog', '>=1.0.14' # for logging
-gem 'jeckyl', '>=0.2.7' # for config files
-gem 'jeni', '>=0.2.5' # for installation
+gem 'jellog', '~> 1.0', '>=1.0.14' # for logging
+gem 'jeckyl', '~> 0.2', '>=0.2.7' # for config files
+gem 'jeni', '~> 0.2', '>=0.2.5' # for installation
 
-gem 'optplus', '>=0.0.2'
+gem 'optplus', '~> 0.0', '>=0.0.2'
--- lib/jerbil/version.rb
+++ lib/jerbil/version.rb
@@ -1,13 +1,13 @@
 # Created by Jevoom
 #
 # 13-Oct-2014
-#   Ensure monitor raises the alarm if the secret key is rejected by another server.
+#   Fix Gemfile dependencies to please rubygems and remove thor as a dependency.
 
 module Jerbil
-  # version set to 1.4.3
-  Version = '1.4.3'
+  # version set to 1.4.4
+  Version = '1.4.4'
   # date set to 13-Oct-2014
   Version_Date = '13-Oct-2014'
-  #ident string set to: jerbil-1.4.3 13-Oct-2014
-  Ident = 'jerbil-1.4.3 13-Oct-2014'
+  #ident string set to: jerbil-1.4.4 13-Oct-2014
+  Ident = 'jerbil-1.4.4 13-Oct-2014'
 end
