0d863d0a5f010a4062e06dbe765786c9fda1669b
Robert Sharp <robert@osburn-sharp.ath.cx>
2014-10-17 09:23:05 +0100

    Fix change to jerbil remotes so that it works!

--- lib/jerbil/version.rb
+++ lib/jerbil/version.rb
@@ -1,14 +1,13 @@
 # Created by Jevoom
 #
 # 17-Oct-2014
-#   Add server version to 'jerbil remotes -V' and add system logging for start and
-#   end of monitor process.
+#   Fix change to jerbil remotes so that it works!
 
 module Jerbil
-  # version set to 1.4.5
-  Version = '1.4.5'
+  # version set to 1.4.6
+  Version = '1.4.6'
   # date set to 17-Oct-2014
   Version_Date = '17-Oct-2014'
-  #ident string set to: jerbil-1.4.5 17-Oct-2014
-  Ident = 'jerbil-1.4.5 17-Oct-2014'
+  #ident string set to: jerbil-1.4.6 17-Oct-2014
+  Ident = 'jerbil-1.4.6 17-Oct-2014'
 end
