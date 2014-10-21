bd25f8ebc8b73d93f68d0018d4dd95afc15863d9
Robert Sharp <robert@osburn-sharp.ath.cx>
2014-10-21 08:00:30 +0100

    Fix readmes on rubydoc.info in the .yardopts file.

--- lib/jerbil/version.rb
+++ lib/jerbil/version.rb
@@ -1,13 +1,13 @@
 # Created by Jevoom
 #
-# 17-Oct-2014
-#   Fix change to jerbil remotes so that it works!
+# 21-Oct-2014
+#   Fix readmes on rubydoc.info in the .yardopts file.
 
 module Jerbil
-  # version set to 1.4.6
-  Version = '1.4.6'
-  # date set to 17-Oct-2014
-  Version_Date = '17-Oct-2014'
-  #ident string set to: jerbil-1.4.6 17-Oct-2014
-  Ident = 'jerbil-1.4.6 17-Oct-2014'
+  # version set to 1.4.7
+  Version = '1.4.7'
+  # date set to 21-Oct-2014
+  Version_Date = '21-Oct-2014'
+  #ident string set to: jerbil-1.4.7 21-Oct-2014
+  Ident = 'jerbil-1.4.7 21-Oct-2014'
 end
